e163a1566d3082980cb88f417d9dd45fb2f442b8
fadlanmawardi <mfadlan0809@gmail.com>
2025-01-23 14:25:14 +0700

    coffe fadlan

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "headers": "url('../img/background2.jpg')"
+      }
+
+    },
+  },
+  plugins: [],
+}
+
